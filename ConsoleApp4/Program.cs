23de5614c49e1b1b95c29526cdc39b9a4c0a0553
ragbonlahor <ragbonlahor@RAGBONLAHO-LTW1>
2023-03-01 14:34:05 +0000

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp4
+{
+    internal class Entry
+    {
+        static void Main(string[] args)
+        {
+            Country[] countries = new Country[3]{
+            new Country("Ireland", "Dublin"),
+            new Country("Nigeria", "Lagos"),
+            new Country("England", "London")
+            };
+            foreach (Country country in countries)
+            {
+                Console.WriteLine(country.capital + " " + country.countryName);
+            }
+        }
+        }
+    class Country
+    {
+        public string countryName = "";
+        public string capital = "";
+        public Country(string countryName, string capital)
+        {
+            this.countryName = countryName;
+            this.capital = capital;
+        }
+    }
+}
+
+
